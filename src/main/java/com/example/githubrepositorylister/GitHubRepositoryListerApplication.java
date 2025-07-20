c4957606717986d49dff608b4f0af58429135428
mart-gant <martgant@gmail.com>
2025-07-20 12:03:14 +0200

    Update

---
+++ src/main/java/com/example/githubrepositorylister/GitHubRepositoryListerApplication.java
@@ -0,0 +1,12 @@
+package com.example.githubrepositorylister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubRepositoryListerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubRepositoryListerApplication.class, args);
+    }
+}
